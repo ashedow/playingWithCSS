2d968b029021b7a3ba09d26bd58297a63c2fffcd
Ya <qsnhywadej@gmail.com>
2017-10-15 17:52:29 +0500

    fix remove

--- main.js
+++ main.js
@@ -2,21 +2,10 @@
 document.getElementById("input").addEventListener("keyup", printChar);
 
 function printChar() {
-	// var char = document.getElementById("input"); 30
+	// var char = document.getElementById("input");
 	var len = document.querySelector('ul').children.length;
-	if (len > 30){
+	if (len > 29){
 		document.querySelector('#output').firstChild.remove()
-		// И чтобы удалить первый, то обращаешься к нему через квадратные скобки [0]
-	// 	if (!Element.prototype.remove) {
- //    		Element.prototype.remove = function remove() {
- //      		if (this.parentNode) {
- //        		this.parentNode.removeChild(this);
- //      			}
- //    		};
- //  		}
-  		// var rem = document.getElementsByClassName('input-success');
-  		// rem.remove();
-  		// output.removeChild(rem)
   	}
 
 	var char = document.getElementById("input").value;
